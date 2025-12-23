b39863d9a24fadd377e25a1fd1a2d20909f5b340
Varun Nair <nairvarun104@gmail.com>
2025-12-23 23:38:15 +0530

    modernize loop

--- AdventOfCode/internal/strutils/transpose.go
+++ AdventOfCode/internal/strutils/transpose.go
@@ -10,9 +10,9 @@ func Transpose(slice []string) []string {
 	NUM_COLS := len(slice)
 	transposed_slice := make([]string, NUM_ROWS)
 
-	for r := 0; r < NUM_ROWS; r++ {
+	for r := range NUM_ROWS {
 		temp := make([]string, NUM_COLS)
-		for c :=  0; c < NUM_COLS; c++ {
+		for c := range NUM_COLS {
 			temp[c] = string(slice[c][r])
 		}
 		transposed_slice[r] = strings.Join(temp, "")
