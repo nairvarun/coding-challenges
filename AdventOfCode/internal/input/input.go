fbf7ba5a70d8e1bcbc06f2ea3a5adbd8747b6954
Varun Nair <nairvarun104@gmail.com>
2025-12-23 23:47:27 +0530

    add input module

--- AdventOfCode/internal/input/input.go
+++ AdventOfCode/internal/input/input.go
@@ -1,29 +1,63 @@
 package input
 
 import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
 	"os"
-	"path/filepath"
-	"strings"
 
-	"github.com/nairvarun/AdventOfCode/internal/projectroot"
+	"github.com/browserutils/kooky"
+	"github.com/browserutils/kooky/browser/safari"
 )
 
-func getFileContent(filename string) (string, error) {
-	example := filepath.Join(projectroot.Path, filename)
+func Fetch(year int, day int) string {
+	dir, _ := os.UserHomeDir()
+	cookiesFile := dir + "/Library/Containers/com.apple.Safari/Data/Library/Cookies/Cookies.binarycookies"
 
-	f, err := os.ReadFile(example)
+	cookies, err := safari.ReadCookies(
+		context.TODO(), 
+		cookiesFile, 
+		kooky.Valid, 
+		kooky.Domain(`.adventofcode.com`), 
+		kooky.Name(`session`),
+	)
+
 	if err != nil {
-		return "", err
+		panic(err)
 	}
 
-	// trim new line and whitespace
-	return strings.Trim(string(f), "\n "), err
-}
+	if len(cookies) == 0 {
+		panic("no session cookie found")
+	}
 
-func Example() (string, error) {
-	return getFileContent("example.txt")
-}
+	sessionCookie := cookies[0].Value
 
-func MyInput() (string, error) {
-	return getFileContent("input.txt")
+	url := fmt.Sprintf("https://adventofcode.com/%d/day/%d/input", year, day)
+	res, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		panic(err)
+	}
+	res.AddCookie(&http.Cookie{
+		Name:  "session",
+		Value: sessionCookie,
+	})
+	res.Header.Set("User-Agent", "github.com/nairvarun/coding-challenges/AdventOfCode by nairvarun104@gmail.com")
+
+	resp, err := http.DefaultClient.Do(res)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		panic(fmt.Sprintf("failed to fetch input: %s", resp.Status))
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	return string(body)
 }
